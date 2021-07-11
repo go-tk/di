05ae586ba17e50376c274b1dafbfb71f09850b82
Roy O'Young <roy2220@outlook.com>
2021-07-11 19:00:25 +0800

    Enhance comments

--- function.go
+++ function.go
@@ -3,6 +3,8 @@ package di
 import "context"
 
 // Function represents the container that describes the specification for dependency injection.
+// Tag serves as additional information to locate the Function for debugging purposes.
+// Body will be called alone with Program.Run().
 type Function struct {
 	Tag       string
 	Arguments []Argument
@@ -12,6 +14,7 @@ type Function struct {
 }
 
 // Argument describes a value the container requires.
+// Function.Body could use the in-value.
 type Argument struct {
 	InValueID  string
 	InValuePtr interface{}
@@ -19,15 +22,20 @@ type Argument struct {
 }
 
 // Result describes a value the container provides.
-// It is optional to do a cleanup for the value when Program.Clean() is executed.
+// Function.Body should provision the out-value.
+// If CleanupPtr is not nil, Function.Body should provision the cleanup.
+// Cleanups are optional and will be called along with Program.Clean() in reverse order of
+// provisioning.
 type Result struct {
 	OutValueID  string
 	OutValuePtr interface{}
 	CleanupPtr  *func()
 }
 
-// Hook describes a callback which should be called once the value is created but
-// has not yet been provisioned to other Functions.
+// Hook describes a callback which should be called once the value is created by a Function
+// (as an out-value) but has not yet been passed to other Functions (as in-values).
+// Function.Body should provision the callback.
+// The callback could use the in-value.
 type Hook struct {
 	InValueID   string
 	InValuePtr  interface{}
