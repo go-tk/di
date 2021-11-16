3cf88801cf6db6d03758e6378b0e5ad274f27f08
Roy O'Young <roy2220@outlook.com>
2021-11-16 17:39:56 +0800

    Add utility function FullFunctionName

--- utils.go
+++ utils.go
@@ -1,16 +1,22 @@
 package di
 
 import (
+	"reflect"
 	"runtime"
 	"strings"
 )
 
+// FullFunctionName returns the full name of the given function.
+func FullFunctionName(f interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+}
+
 // PackagePath returns the package path of the caller.
 func PackagePath(skip int) string {
 	pc, _, _, _ := runtime.Caller(skip + 1)
-	functionFullName := runtime.FuncForPC(pc).Name()
-	i := strings.LastIndexByte(functionFullName, '.')
-	packagePath := functionFullName[:i]
+	fullFunctionName := runtime.FuncForPC(pc).Name()
+	i := strings.LastIndexByte(fullFunctionName, '.')
+	packagePath := fullFunctionName[:i]
 	return packagePath
 }
 
--- utils_test.go
+++ utils_test.go
@@ -7,6 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestFullFunctionName(t *testing.T) {
+	assert.Equal(t, "github.com/go-tk/di_test.TestFullFunctionName", FullFunctionName(TestFullFunctionName))
+}
+
 func TestPackagePath(t *testing.T) {
 	assert.Equal(t, "github.com/go-tk/di_test", PackagePath(0))
 }
